80f3a0d040cb70617d530608711d4dd7886c0a5f
rafanetow <rafanetow@gmail.com>
2019-07-18 12:00:28 -0700

    adding general code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ButtonAppBar from './components/Menu'
+import General from './components/StudentInformation/General'
 import HomePage from './components/HomePage'
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PageNotFound from "./PageNotFound";
@@ -13,6 +14,7 @@ class App extends React.Component {
    <BrowserRouter>
    <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/studentinformation/general" component={General} />
         <Route component={PageNotFound} />
       </Switch>
       </BrowserRouter>
